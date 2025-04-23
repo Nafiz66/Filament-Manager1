9f4eb81a875eaa02c45f633ce6ba8cd1f3eaf970
Nafiz66 <alaturka88.na@gmail.com>
2025-04-23 08:23:15 +0200

    Projektdateien hinzufügen.

---
+++ Projekt Nafiz/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Filament> filamentListe = new List<Filament>();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n📦 Filament-Manager");
+            Console.WriteLine("1. Filament hinzufügen");
+            Console.WriteLine("2. Filamentliste anzeigen");
+            Console.WriteLine("0. Beenden");
+            Console.Write("Wähle eine Option: ");
+            string eingabe = Console.ReadLine();
+
+            switch (eingabe)
+            {
+                case "1":
+                    Filament neues = new Filament();
+                    Console.Write("Name: ");
+                    neues.Name = Console.ReadLine();
+                    Console.Write("Farbe: ");
+                    neues.Farbe = Console.ReadLine();
+                    Console.Write("Material: ");
+                    neues.Material = Console.ReadLine();
+                    Console.Write("Gewicht (g): ");
+                    neues.Gewicht = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Preis pro kg (€): ");
+                    neues.PreisProKg = Convert.ToDouble(Console.ReadLine());
+
+                    filamentListe.Add(neues);
+                    Console.WriteLine("✅ Filament gespeichert!");
+                    break;
+
+                case "2":
+                    Console.WriteLine("\n📋 Aktuelle Filamente:");
+                    foreach (var f in filamentListe)
+                    {
+                        Console.WriteLine($"- {f.Name} | {f.Farbe} | {f.Material} | {f.Gewicht}g | {f.PreisProKg} €/kg");
+                    }
+                    break;
+
+                case "0":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("❌ Ungültige Eingabe");
+                    break;
+            }
+        }
+    }
+}
---
+++ Projekt Nafiz/public class Filament.cs
@@ -0,0 +1,8 @@
+﻿public class Filament
+{
+    public string Name { get; set; }
+    public string Farbe { get; set; }
+    public string Material { get; set; }
+    public double Gewicht { get; set; } // in Gramm
+    public double PreisProKg { get; set; } // z. B. 20€/kg
+}
