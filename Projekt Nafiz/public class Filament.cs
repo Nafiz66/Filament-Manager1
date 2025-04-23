89d6b9c35f0b8c153056b345d3259b210fbe49c0
Nafiz66 <alaturka88.na@gmail.com>
2025-04-23 10:54:33 +0200

    klasse hinzugefügt

--- Projekt Nafiz/public class Filament.cs
+++ Projekt Nafiz/public class Filament.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace FilamentKostenRechner
+{
+    public class Filament
+    {
+        public string Name { get; set; }
+        public string Marke { get; set; }
+        public double PreisProKg { get; set; }
+
+        public Filament(string name, string marke, double preisProKg)
+        {
+            Name = name;
+            Marke = marke;
+            PreisProKg = preisProKg;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} von {Marke} - {PreisProKg} €/kg";
+        }
+    }
+}
